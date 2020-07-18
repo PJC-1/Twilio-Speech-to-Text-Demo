f911be10ba73554d9949187fbada94c3eb9789a2
PJC-1 <jumpzine@gmail.com>
2020-07-18 11:24:57 -0700

    added start stop and onresult functions

--- script.js
+++ script.js
@@ -12,5 +12,37 @@ window.addEventListener("DOMContentLoaded", () => {
     message.setAttribute("aria-hidden", "false");
   } else {
     console.log("supported browser");
+    let listening = false;
+    const recognition = new SpeechRecognition();
+    const start = () => {
+      recognition.start();
+      button.textContent = "Stop listening";
+      main.classList.add("speaking");
+    };
+    const stop = () => {
+      recognition.stop();
+      button.textContent = "Start listening";
+      main.classList.remove("speaking");
+    };
+    const onResult = event => {
+      result.innerHTML = "";
+      for (const res of event.results) {
+        const text = document.createTextNode(res[0].transcript);
+        const p = document.createElement("p");
+        if (res.isFinal) {
+          p.classList.add("final");
+        }
+        p.appendChild(text);
+        result.appendChild(p);
+      }
+    };
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.addEventListener("result", onResult);
+
+    button.addEventListener("click", () => {
+      listening ? stop() : start();
+      listening = !listening;
+    });
   }
 });
